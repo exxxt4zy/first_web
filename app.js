c50d1f973f86cd434d82ab6d58e55a179ac6a79a
exxxt4zy <exxxt4zy@gmail.com>
2022-01-20 00:11:53 +0500

    m1s1

---
+++ app.js
@@ -0,0 +1,23 @@
+const textHours = document.getElementById('hours'),
+    textMinutes = document.getElementById('minutes'),
+    textDivide = document.getElementById('divide')
+
+const clockText = () => {
+    let date = new Date()
+
+    let hh = date.getHours(),
+        mm = date.getMinutes()
+
+    if (hh == 0) {hh = 12}
+    if (hh < 10) {hh = `0${hh}`}
+    if (mm < 10) {mm = `0${mm}`}
+    textMinutes.innerHTML = mm
+    textHours.innerHTML = `${hh}`
+}
+setInterval (clockText, 1000)
+
+const btn = document.getElementById("btn-toggle");
+
+btn.addEventListener("click", function () {
+    document.body.classList.toggle("light-theme");
+});
